1c6d0aae423a2748af7deb8dd0dc96e432e399c9
beogenius <70867972+beogenius@users.noreply.github.com>
2021-02-03 11:25:35 +0700

    update layout va router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,47 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {LoginComponent} from "./login/login.component";
+import {LayoutComponent} from "./layout/layout.component";
+import {NewfeedsComponent} from "./layout/newfeeds/newfeeds.component";
+import {FriendComponent} from "./layout/friend/friend.component";
+import {PersonalpageComponent} from "./layout/personalpage/personalpage.component";
 
-const routes: Routes = [];
+export const routes: Routes = [
+  // {
+  //   path: '',
+  //   redirectTo: 'login',
+  //   pathMatch: 'full',
+  // },
+  {
+    path: 'login',
+    component: LoginComponent,
+    data: {
+      title: 'Login List',
+    },
+  },
+  {
+    path: 'index',
+    component: LayoutComponent,
+    data: {
+      title: 'Index'
+    },
+    children: [
+      {
+        path: 'newfeeds',
+        component: NewfeedsComponent ,
+      },
+      {
+        path: 'friends',
+        component: FriendComponent,
+      },
+      {
+        path: 'personal',
+        component: PersonalpageComponent,
+      },
+
+    ]
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,34 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
 import {LayoutComponent} from './layout/layout.component';
+import {NewfeedsComponent} from './layout/newfeeds/newfeeds.component';
+import {FriendComponent} from './layout/friend/friend.component';
+import {HttpClientModule} from "@angular/common/http";
+import { PersonalpageComponent } from './layout/personalpage/personalpage.component';
+import { NavbarComponent } from './layout/navbar/navbar.component';
+
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        LayoutComponent
-    ],
+  declarations: [
+    AppComponent,
+    LayoutComponent,
+    NewfeedsComponent,
+    FriendComponent,
+    PersonalpageComponent,
+    NavbarComponent
+  ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+export class AppModule {
+
+}
