53e2dfe77a52f9e3c1abbc9eb3f3a30f53433910
beogenius <70867972+beogenius@users.noreply.github.com>
2021-02-16 18:18:49 +0700

    Add friend in layout and logout button

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Friend} from "../model/friend";
+import {FriendShipService} from "../services/friendshipservice";
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-layout',
@@ -6,11 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./layout.component.css']
 })
 export class LayoutComponent implements OnInit {
-  username: any = sessionStorage.getItem('AuthUsername');
+  username: any;
+  friends: Friend[] = [];
 
-  constructor() { }
+  constructor(private route: ActivatedRoute,
+              private router: Router, private friendShipService: FriendShipService) { }
 
   ngOnInit(): void {
+    this.reloaddata();
   }
 
+  async reloaddata() {
+    this.username = sessionStorage.getItem('AuthUsername');
+    let friendResponse = await this.getFriendList(this.username);
+    this.friends = friendResponse.data;
+  };
+
+
+  goToPersonal(username: any) {
+    this.router.navigate(['index/personal',username])
+  }
+
+  getFriendList(username: any) {
+    return this.friendShipService.getFriendList(username).toPromise();
+  };
 }
