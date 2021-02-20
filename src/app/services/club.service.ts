8ce23d62aa460b5929d2b27c366eca33d2263d2e
HoangVietAnh <hoangvietanh1797@gmail.com>
2021-02-20 11:16:37 +0700

    update club service

---
+++ src/app/services/club.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Club} from '../model/dung/club';
+@Injectable({
+  providedIn: 'root'
+})
+export class ClubService {
+  url = 'http://localhost:8080/club/';
+  constructor(private http: HttpClient) { }
+  getClubListByUserCreate(username: any):Observable<any>{
+    return this.http.get(this.url + username + '/listclubbyusercreate')
+  }
+  createClub(club: Club,username: any):Observable<any>{
+    return this.http.post(this.url + username + '/create',club)
+  }
+  getClubNotJoinedYet(username: any):Observable<any>{
+    return this.http.get(this.url + username + '/getclubsnotjoinedyet')
+  }
+  deleteClub(username: any,clubId: any):Observable<any>{
+    return this.http.delete(this.url + username +'/deleteclub/' + clubId)
+  }
+}
