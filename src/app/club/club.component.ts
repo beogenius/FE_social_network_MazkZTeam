563ae88774551dcac40932c3a99e9707258cbcfb
HoangVietAnh <hoangvietanh1797@gmail.com>
2021-02-20 11:15:03 +0700

    show group list, add delete group

--- src/app/club/club.component.ts
+++ src/app/club/club.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import {Club} from '../model/dung/club';
+import {ActivatedRoute, Router} from '@angular/router';
+import {FriendShipService} from '../services/friendshipservice';
+import {ClubService} from '../services/club.service';
+import {User} from '../model/dung/user';
+import Swal from "sweetalert2";
+import {migrateLegacyGlobalConfig} from '@angular/cli/utilities/config';
 
 @Component({
   selector: 'app-club',
@@ -6,10 +13,97 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./club.component.css']
 })
 export class ClubComponent implements OnInit {
+  listClubByUserCreate: Club[] = [];
+  listClubNotJoinedYet: Club[] = [];
+  permissionx='1';
+  user: User = {};
+  newClub: Club = {};
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private router: Router,
+              private sv: ClubService) {
   }
 
+  ngOnInit(): void {
+    // @ts-ignore
+    this.user.username = sessionStorage.getItem('AuthUsername');
+    this.reloaddata();
+  }
+
+  async reloaddata() {
+    let listClubByUserCreate = await this.getListClubByUserCreate(this.user.username)
+    this.listClubByUserCreate = listClubByUserCreate;
+    let listClubNotJoinedYet = await this.getListClubNotJoinedYet(this.user.username);
+    this.listClubNotJoinedYet = listClubNotJoinedYet;
+  }
+
+
+  getListClubByUserCreate(username: any) {
+    return this.sv.getClubListByUserCreate(username).toPromise();
+  }
+  getListClubNotJoinedYet(username: any){
+    return this.sv.getClubNotJoinedYet(username).toPromise();
+  }
+
+  goToClub(name: string) {
+
+  }
+
+  createClub(club: Club) {
+    this.sv.createClub(club, this.user.username).subscribe(
+      res => {
+        this.listClubByUserCreate.unshift(res);
+        Swal.fire({
+          icon: 'success',
+          title: 'Create successfully!',
+          showConfirmButton: false,
+          timer: 500
+        });
+        this.newClub.name = '';
+        this.newClub.detail = '';
+        this.permissionx = '1';
+      },
+      error => {
+        alert('false to create');
+      }
+    );
+  }
+
+  onSubmit() {
+    if (this.permissionx == "1"){
+      this.newClub.permission = 1;
+    }else{
+      this.newClub.permission = 2;
+    }
+    console.log(this.newClub);
+    this.createClub(this.newClub);
+
+  }
+
+  deleteClub(username: any,clubId: any) {
+    Swal.fire({
+      title: 'Are you sure to delete group ?',
+      text: 'You won\'t be able to revert this!',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.sv.deleteClub(username,clubId).subscribe(data => {
+          this.reloaddata();
+          Swal.fire(
+            'Delete Group!',
+            'Your Group has been delete.',
+            'success'
+          );
+        });
+      }
+    });
+    console.log(username, clubId);
+  }
+
+  joinClub() {
+
+  }
 }
